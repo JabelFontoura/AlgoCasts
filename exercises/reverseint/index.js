02a03539319787321ab3f43a43289528f546689c
Jabel Fontoura <jabel_fontoura@hotmail.com>
2018-03-21 14:11:46 -0300

    Solutions for reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,34 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+module.exports = function reverseInt(n) {
+    // return solutionOne(n);
+    // return solutionTwo(n);
+    return solutionThree(n);
+}
 
-module.exports = reverseInt;
+function solutionOne(n) {
+    const negative = n.toString().indexOf('-') !== -1;
+    let reversed = Math.abs(n).toString().split('').reverse().join('');
+
+    reversed = negative ? `-${reversed}` : reversed;
+
+    return parseInt(reversed);
+}
+
+function solutionTwo(n) {
+    const reversed = n.toString().split('').reverse();
+
+    if (Math.sign(n) === -1) {
+        reversed.splice(0, 0, reversed[reversed.length - 1]);
+        reversed.splice(reversed.length - 1 , 1);
+    }
+    
+    return Number(reversed.join(''));
+}
+
+function solutionThree(n) {
+    const reversed = n.toString().split('').reverse().join('');
+
+    return parseInt(reversed) * Math.sign(n);
+}
