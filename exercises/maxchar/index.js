d31f7049aea291762ee8762ebb8534dbe5ab1ce1
Jabel Fontoura <jabel_fontoura@hotmail.com>
2018-03-22 09:14:13 -0300

    Solution for max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+module.exports = function maxChar(str) {
+    return solutionOne(str);
+}
 
-module.exports = maxChar;
+function solutionOne(str) {
+    const map = [];
+
+    for (let char of str) {
+        const existent = map.find(item => item.char === char);
+
+        if (existent) 
+            existent.count++;
+        else
+            map.push({ char, count: 1 });
+    }
+
+    return map.reduce((a, b) => a.count > b.count ? a : b).char;
+}
